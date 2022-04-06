fef45dd72cf5e6389141317f9fc196abbb044eaf
Jay <jairam.kakar23@gmail.com>
2022-04-06 17:26:30 -0400

    Worked on HomeFragment some optional requirements added;

--- Aryan1Jairam2Assign44/src/main/java/aryan1/jairam2/codemasters/ui/home/HomeFrag.kt
+++ Aryan1Jairam2Assign44/src/main/java/aryan1/jairam2/codemasters/ui/home/HomeFrag.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextClock
-import android.widget.TextView
+import android.content.Context
+import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import aryan1.jairam2.codemasters.R
 import aryan1.jairam2.codemasters.databinding.HomeFragmentBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 import java.time.LocalDate
 
 class HomeFrag : Fragment() {
+
     private var binding: HomeFragmentBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +27,42 @@ class HomeFrag : Fragment() {
         val currentDate= LocalDate.now()
         textView.text = currentDate.toString()
         //val textClock : TextClock = binding!!.jairamTC1
+        val storeBtn : Button = binding!!.button
 
+        storeBtn.setOnClickListener(){
+            createAndAdd()
+            val file = File(getText(R.string.jayAryanET).toString())
+            val fExist = file.exists()
+            val editText : EditText = binding!!.jairamET1
+            editText.text.clear()
+            if(fExist){
+                addToExisting()
+            }else{
+                createAndAdd()
+            }
+        }
         return root
+    }
+
+    private fun addToExisting(){
+        val file = File(getText(R.string.jayAryanET).toString())
+        val editText : EditText = binding!!.jairamET1
+        file.appendText(editText.text.toString())
+        Toast.makeText(activity, getText(R.string.addTxtSuc), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun createAndAdd() {
+        try {
+            val editText : EditText = binding!!.jairamET1
+            val fileOutputStream: FileOutputStream = requireActivity().openFileOutput(getText(R.string.jayAryanET).toString(), Context.MODE_PRIVATE)
+            val outputWriter = OutputStreamWriter(fileOutputStream)
+            outputWriter.append(editText.text.toString())
+            outputWriter.close()
+            Toast.makeText(activity, getText(R.string.addTxtSuc), Toast.LENGTH_SHORT).show()
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun onDestroyView() {
